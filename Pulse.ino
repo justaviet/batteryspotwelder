3b903b77a2fa386b0c28d4da8d8731fd5f5f54cc
Minh Vu <32403978+justaviet@users.noreply.github.com>
2020-05-08 03:41:53 -0500

    Update Pulse.ino

--- Pulse.ino
+++ Pulse.ino
@@ -1,7 +1,23 @@
-void_setup(){
+float pulseDuration = 0.0;
+const int buttonPin = 2;
+const int relay = 3;
+int buttonState = 0;
 
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(relay, OUTPUT);
+  Serial.begin(9600);
 }
 
-void_loop(){
-
+void loop() {
+  pulseDuration = analogRead(A0)*(5.0/1023.0);
+  buttonState = digitalRead(buttonPin);
+  Serial.print("Pulse Duration: ");
+  Serial.print(pulseDuration);
+  Serial.println(" ms");
+  if(buttonState == HIGH){
+    digitalWrite(relay, HIGH);
+    delay(pulseDuration);
+    digitalWrite(relay, LOW);
+  }
 }
